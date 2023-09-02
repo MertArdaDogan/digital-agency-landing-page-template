9fa8fcc1c246ee58f3b4d9740a916db9de4cf4c6
Mert <dmert3153@gmail.com>
2023-09-02 14:36:06 +0300

    add

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,10 @@
-const sr = ScrollReveal({
-    // distance: "35px",
-    duration: 2400,
-  });
-  sr.reveal(".container", {
-    delay: 150,
-  });
-  sr.reveal("ul", {
-    delay: 150,
-  });
+// const sr = ScrollReveal({
+//     // distance: "35px",
+//     duration: 2400,
+//   });
+//   sr.reveal(".container", {
+//     delay: 150,
+//   });
+//   sr.reveal("ul", {
+//     delay: 150,
+//   });
